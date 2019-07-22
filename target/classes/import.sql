23d9a9e990d31ede5a6a9bcca8c605534e63cb47
Matías Benjamín Moreno Juárez <morenobmatias@gmail.com>
2019-07-22 20:26:47 +0100

    Sql Data
    Testing data

---
+++ target/classes/import.sql
@@ -0,0 +1,13 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05', null);
+INSERT INTO clientes (nombre, apellido, email, create_at, delete_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06', null);
